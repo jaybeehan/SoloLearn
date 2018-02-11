b337fe1f811180534cb5f06edddb722723b18cbc
jasonhancock <jason.hancock@gmail.com>
2018-02-11 13:53:02 -0700

    Start of project

---
+++ SoloLearnNumOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoloLearnNumOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
